4f353bf9f9bce2c9acb96ebc08525120aec94d94
Oliver <107921797+oliverglo@users.noreply.github.com>
2025-11-09 15:23:09 +0100

    new paragraph fonts

--- build.js
+++ build.js
@@ -139,8 +139,7 @@ function processMarkdownFile(filePath) {
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,400;0,600;1,400;1,600&display=swap" rel="stylesheet">
-        <link href="https://fonts.cdnfonts.com/css/geist" rel="stylesheet">
+        <link href="https://fonts.cdnfonts.com/css/geist?weight=400;600" rel="stylesheet">
         <link rel="stylesheet" href="styles.css?v=${Date.now()}">
         <title>${title}</title>
         <!-- Cloudflare Web Analytics -->
